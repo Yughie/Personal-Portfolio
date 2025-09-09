de97e3e1acd503a997f1cf36241eb2badbdb6266
Yughie <yughiep@gmail.com>
2025-09-09 22:40:00 +0800

    Finished Tech Stack Update

--- portfolio-frontend/src/App.jsx
+++ portfolio-frontend/src/App.jsx
@@ -1,18 +1,16 @@
-import { useState } from "react";
 import "./App.css";
-import ContactForm from "./components/ContactFrom";
 import Header from "./components/Header";
 import { Hero } from "./components/hero";
-import { TechStack } from "./components/TechStack";
+import TechStack from "./components/TechStack";
+import Career from "./components/Career";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="App">
         <Header></Header>
         <Hero></Hero>
+        <TechStack></TechStack>
       </div>
     </>
   );
---
+++ portfolio-frontend/src/components/TechStack.jsx
@@ -0,0 +1,103 @@
+import LogoLoop from "./animation/LogoLoop";
+
+const imageLogos = [
+  {
+    src: "/public/TechStack/Html.svg",
+    alt: "HTML5",
+    href: "https://www.w3schools.com/Html/",
+  },
+  {
+    src: "/public/TechStack/css.svg",
+    alt: "CSS",
+    href: "https://www.w3schools.com/css/",
+  },
+  {
+    src: "/public/TechStack/JavaScript.svg",
+    alt: "JavaScript",
+    href: "https://www.w3schools.com/js/",
+  },
+  {
+    src: "/public/TechStack/TailwindCss.svg",
+    alt: "Tailwind CSS",
+    href: "https://www.geeksforgeeks.org/css/tailwind-css/",
+  },
+  {
+    src: "/public/TechStack/ReactJs.svg",
+    alt: "React JS",
+    href: "https://www.geeksforgeeks.org/reactjs/react/",
+  },
+  {
+    src: "/public/TechStack/NodeJs.svg",
+    alt: "React JS",
+    href: "https://www.geeksforgeeks.org/node-js/nodejs/",
+  },
+  {
+    src: "/public/TechStack/Firebase.svg",
+    alt: "React JS",
+    href: "https://www.geeksforgeeks.org/firebase/firebase-tutorial/",
+  },
+  {
+    src: "/public/TechStack/Php.svg",
+    alt: "React JS",
+    href: "https://www.w3schools.com/php/",
+  },
+  {
+    src: "/public/TechStack/Laravel.svg",
+    alt: "React JS",
+    href: "https://www.geeksforgeeks.org/php/introduction-to-laravel-and-mvc-framework/",
+  },
+  {
+    src: "/public/TechStack/Sql.svg",
+    alt: "React JS",
+    href: "https://www.w3schools.com/sql/",
+  },
+  {
+    src: "/public/TechStack/MySql.svg",
+    alt: "React JS",
+    href: "https://www.w3schools.com/MySQL/",
+  },
+];
+
+const TechStack = () => {
+  return (
+    <div className="tech-stack-section py-20 mt-10 md:mt-0 lg:py-0 bg-gray-50">
+      <h2 className="text-3xl font-bold text-center mb-10 text-brandblack">
+        TECH STACK
+      </h2>
+      <div
+        style={{ height: "100px", position: "relative", overflow: "hidden" }}
+      >
+        <LogoLoop
+          logos={imageLogos}
+          speed={60}
+          direction="left"
+          logoHeight={80}
+          gap={40}
+          pauseOnHover
+          scaleOnHover
+          fadeOut
+          fadeOutColor="#ffffff"
+          ariaLabel="Technology Stack"
+        />
+      </div>
+      <div
+        style={{ height: "100px", position: "relative", overflow: "hidden" }}
+      >
+        <LogoLoop
+          logos={imageLogos}
+          speed={60}
+          direction="rigth"
+          logoHeight={80}
+          gap={40}
+          pauseOnHover
+          scaleOnHover
+          fadeOut
+          fadeOutColor="#ffffff"
+          ariaLabel="Technology partners"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default TechStack;
