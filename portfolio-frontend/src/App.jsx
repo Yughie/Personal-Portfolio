0ce66701afe0321848f893391d5e626313c76a16
Yughie <yughiep@gmail.com>
2025-09-19 00:07:26 +0800

    Debug: Test

--- portfolio-frontend/src/App.jsx
+++ portfolio-frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import "./App.css";
 import Header from "./components/Header.jsx";
 import { Hero } from "./components/hero.jsx";
-import TechStack from "./components/TechStack.jsx";
-import Career from "./components/Career.jsx";
 import Showcase from "./components/Showcase.jsx";
 import ContactForm from "./components/ContactForm.jsx";
 import Footer from "./components/Footer.jsx";
@@ -13,8 +11,6 @@ function App() {
       <div className="App">
         <Header></Header>
         <Hero></Hero>
-        <TechStack></TechStack>
-        <Career></Career>
         <Showcase></Showcase>
         <ContactForm></ContactForm>
         <Footer></Footer>
