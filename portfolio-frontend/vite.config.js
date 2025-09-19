4029883e59a501f768dd403bb1d667c3bed8814d
Yughie <yughiep@gmail.com>
2025-09-19 23:59:37 +0800

    Small vercel Update

--- portfolio-frontend/vite.config.js
+++ portfolio-frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL || '/Personal-Portfolio',
+  base: process.env.VITE_BASE_URL || '/personal-portfolio',
   optimizeDeps: {
     include: ['prop-types']
   }
